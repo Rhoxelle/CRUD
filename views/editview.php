ac7abfa102a9173d00c9e68ad4b703ee7d9771cf
rhoxelle <rhoxelle.support.ph@tripleonetech.net>
2023-04-12 07:17:59 +0800

    refactor to PDO

--- views/editview.php
+++ views/editview.php
@@ -1,30 +1,3 @@
-<?php
-require "db.conn.php";
-
-if(isset($_POST['submit'])) {
-    $id = $_POST['id'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $contact_number = $_POST['contact_number'];
-    $gender = $_POST['gender'];
-    $marital_status = $_POST['marital_status'];
-    $department = $_POST['department'];
-    $position = $_POST['position'];
-
-    $sql = "UPDATE `crud` SET `first_name`='$first_name',`last_name`='$last_name',`email`='$email',`contact_number`='$contact_number',`gender`='$gender',`marital_status`='$marital_status',`department`='$department',`position`='$position' WHERE id={$id}";
-
-    $result = mysqli_query($conn, $sql);
-    
-    if($result) {
-        header('Location: index.php?msg=Updated user information successfully.');
-    }
-    else {
-        echo "Failed: " . mysqli_error($conn);
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,38 +24,30 @@ if(isset($_POST['submit'])) {
              <p class="text-muted">Click udate after changing any information.</p>
         </div>
 
-        <?php
-          require "db.conn.php";
-
-        $id = $_GET['id'];
-        $sql = "SELECT * FROM `crud` WHERE id = $id LIMIT 10";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
-        ?>
-
         <div class="container d-flex justify-content-center">
-            <form action="" method="post" style="width:50vwl; min-width:300px;">
+            <form action="../functions/edit.php" method="post" style="width:50vwl; min-width:300px;">
                 <div hidden>
-                    <input type="text" class="form-control" name="id" value="<?php echo $row['id']?>">
+                    <input type="text" class="form-control" name="id" value="<?= $result->id ?>">
                 </div>    
                 
                  <div class="row mb-2">
                       <div class="col">
                         <label class="form.label">First Name:</label>
-                        <input type="text" class="form-control" name="first_name" value="<?php echo $row['first_name']?>">
+                        <input type="text" class="form-control" name="first_name" value="<?= $result->first_name ?>">
                       </div>
 
                       <div class="col">
                         <label class="form.label">Last Name:</label>
-                        <input type="text" class="form-control" name="last_name" value="<?php echo $row['last_name']?>">
+                        <input type="text" class="form-control" name="last_name" value="<?= $result->last_name ?>">
                       </div>
                  </div>
 
                  <div class="form-group mb-2">
                            <label for="department">Department:</label> &nbsp;
                             <select name="department" id="department">
-                            <option value="" selected></option> &nbsp;
+                            <option value="<?= $result->department ?>" selected><?= $result->department ?></option> &nbsp;
                             <option value="Business Analyst">Business Analyst</option> &nbsp;
+                            <!-- Disable if Selected -->
                             <option value="Quality Assurance">Quality Assurance</option> &nbsp;
                             <option value="Dev Support">Dev Support</option> &nbsp;
                             <option value="PHP Dev">PHP Dev</option> &nbsp;
@@ -93,11 +58,23 @@ if(isset($_POST['submit'])) {
                             </select>
                 </div>
 
+                <div class="form-group mb-2">
+                           <label for="position">Position:</label> &nbsp;
+                            <select name="position" id="position">
+                            <option value="<?= $result->position ?>" selected><?= $result->position ?></option> &nbsp;
+                            <option value="Junior">Junior</option> &nbsp;
+                            <!-- Disable if Selected -->
+                            <option value="Senior">Senior</option> &nbsp;
+                            <option value="Team Leader">Team Leader</option> &nbsp;
+                            </select>
+                 </div>
+
                  <div class="form-group mb-2">
                            <label for="marital_status">Marital Status:</label> &nbsp;
                             <select name="marital_status" id="status">
-                            <option value="Single" selected>Single</option> &nbsp;
-                            <option value="Marrieds">Married</option> &nbsp;
+                            <option value="<?= $result->marital_status ?>" selected><?= $result->marital_status ?></option> &nbsp;
+                            <option value="Single">Single</option> &nbsp;
+                            <option value="Married">Married</option> &nbsp;
                             <option value="Divorced">Divorced</option> &nbsp;
                             <option value="xWidowed">Widowed</option> &nbsp;
                             </select>
@@ -105,30 +82,25 @@ if(isset($_POST['submit'])) {
 
                 <div class="form-group mb-2">
                            <label>Gender:</label> &nbsp;
-                           <input type="radio" class="form-check-input" name="gender" id="male" value="Male" <?php echo ($row['gender']=='male')?"checked":""?>>
+                           <input type="radio" class="form-check-input" name="gender" id="male" value="Male" <?= $result->marital_status='male'?"checked":"" ?>>
                            <label for="Male">Male</label> &nbsp;
-                           <input type="radio" class="form-check-input" name="gender" id="female" value="Female" <?php echo ($row['gender']=='female')?"checked":""?>>
+                           <input type="radio" class="form-check-input" name="gender" id="female" value="Female" <?= $result->marital_status='female'?"checked":"" ?>>
                            <label for="Female">Female</label> &nbsp;
                 </div>
 
                  <div class="mb-2">
                  <label class="form.label">Email:</label>
-                 <input type="text" class="form-control" name="email" value="<?php echo $row['email']?>">
+                 <input type="text" class="form-control" name="email" value="<?= $result->email ?>">
                  </div>
 
                  <div class="mb-2">
                  <label class="form.label">Contact Number:</label>
-                 <input type="number" class="form-control" name="contact_number" value="<?php echo $row['contact_number']?>">
-                 </div>
-
-                 <div class="mb-2">
-                        <label class="form.label">Position:</label>
-                        <input type="text" class="form-control" name="position" value="<?php echo $row['position']?>">
+                 <input type="number" class="form-control" name="contact_number" value="<?= $result->contact_number ?>">
                  </div>
 
                  <div>
                     <button type="submit" class="btn btn-success" name="submit">Update</button>
-                    <a href="index.php" class="btn btn-danger">Back</a>
+                    <a href="../index.php" class="btn btn-danger">Back</a>
                 </div>
 
              </form>
